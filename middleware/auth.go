ffea08e79481642fe619884a9a8a4328529c71fd
Tyler Sanichara <Tsanichara67@gmail.com>
2024-11-24 11:05:02 -0500

    Added route protection for updateEvent and deleteEvent

---
+++ middleware/auth.go
@@ -0,0 +1,27 @@
+package middleware
+
+import (
+	"net/http"
+
+	"github.com/Tsanichara/Go_REST_API/utils"
+	"github.com/gin-gonic/gin"
+)
+
+func Authenticate(context *gin.Context) {
+	token := context.Request.Header.Get("Authorization")
+
+	if token == "" {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not Authorized"})
+		return
+	}
+
+	userId, err := utils.VerifyToken(token)
+
+	if err != nil {
+		context.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"message": "Not Authorized"})
+	}
+
+	context.Set("userId", userId)
+
+	context.Next()
+}
